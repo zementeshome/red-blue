e9ce1434d8d57b3e11f9c876008e47eb4c5759c4
Zemen Teshome <zementeshome@gmail.com>
2021-01-24 19:55:30 -0500

    firebase set up

---
+++ client/src/components/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import fire from '../../fire';
+
+function Login() {
+    return (
+        <section className="login">
+            
+        </section>
+    )
+}
+
+export default Login;
---
+++ client/src/fire.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase';
+import 'firebase/firestore';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAzvHi2h3OtQ17gGZmiFYxhrqzXWmFn3jw",
+    authDomain: "red-blue-mh.firebaseapp.com",
+    projectId: "red-blue-mh",
+    storageBucket: "red-blue-mh.appspot.com",
+    messagingSenderId: "135834074932",
+    appId: "1:135834074932:web:980403bd87df270f88af61"
+  };
+
+  const fire = firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  export const db = fire.firestore();
+  export const auth = firebase.auth();
+
+  firebase.auth().setPersistance(firebase.auth.Auth.Persistance.NONE)
+
+  export default fire;
