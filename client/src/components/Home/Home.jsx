651d6bdc80a865206ed1f1dfdbc84d979dfe7dc2
Zemen Teshome <zementeshome@gmail.com>
2021-01-24 22:40:43 -0500

    logic forred or blue ball

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import fire from '../../fire';
 import { useHistory } from 'react-router-dom';
+import './Home.scss';
 
 function Home() {
 const history = useHistory()
@@ -10,10 +11,19 @@ const handleLogout = () => {
     })
 }
 
+let color = "blue";
+if (Math.random() < 0.5) {
+    color = "red"
+}
+
+
     return (
         <section className="home">
             <h1>Home</h1>
             <button className="home__logout" onClick={handleLogout}>logout</button>
+            <div className="home__circle-container">
+            <div className="home__circle" style={{background: color}}></div>
+            </div>
         </section>
     )
 }
