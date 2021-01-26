cbc7b7d078063efc66c16c9135926a0985252674
Zemen Teshome <zementeshome@gmail.com>
2021-01-26 10:13:12 -0500

    images instead of colour now displaying and saving as user objects

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,68 +1,106 @@
-import React from 'react'
-import fire from '../../fire';
-import { useHistory } from 'react-router-dom';
-import './Home.scss';
+import React, { useEffect, useState } from "react";
+import fire from "../../fire";
+import { useHistory } from "react-router-dom";
+import "./Home.scss";
 // import Cookies from 'js-cookie';
-import earth from '../../assets/images/earth.png';
-import mars from '../../assets/images/Planet_Mars.png';
+import earth from "../../assets/images/earth.png";
+import mars from "../../assets/images/Planet_Mars.png";
 
 function Home() {
-const history = useHistory()
-const handleLogout = () => {
-    fire.auth().signOut().then(() => {
-        history.push('/');
-    })
-}
+  const [image, setImage] = useState("");
+  const [timesViewed, setTimesViewed] = useState(null);
+  const history = useHistory();
+  const handleLogout = () => {
+    fire
+      .auth()
+      .signOut()
+      .then(() => {
+        history.push("/");
+      });
+  };
 
-const earthOrMars = new Array('../../assets/images/earth.png', '../../assets/images/Planet_Mars.png')
+  // const earthOrMars = new Array('../../assets/images/earth.png', '../../assets/images/Planet_Mars.png')
 
+  function displayImage() {
+    return Math.random() > 0.5 ? mars : earth; // fix this
+    // return Math.floor(Math.random() * earthOrMars.length / 2)
+  }
 
-function displayImage() {
-    // return Math.random() > 0.5 ? earth : mars
-    return Math.floor(Math.random() * earthOrMars.length / 2)
-}
-
-function getCookie(key) {
-    const pattern = new RegExp("(?:(?:^|.*;\\s*)" + key + "\\s*\\=\\s*([^;]*).*$)|^.*$")
+  function getCookie(key) {
+    const pattern = new RegExp(
+      "(?:(?:^|.*;\\s*)" + key + "\\s*\\=\\s*([^;]*).*$)|^.*$"
+    );
     return document.cookie.replace(pattern, "$1");
   }
 
+  function updateImageCookies(user) {
+    // {
+    //     'test@gamil':{
+    //         earth_orm:
+    //         image_view:2
+    //     },
 
-function updateImageCookies() {
-    let image = getCookie('earth_or_mars');
-    let timesViewed;
+    // }
+    
+    if (user) {
+      let usersCookie = getCookie("users");
+      let users = usersCookie ? JSON.parse(getCookie("users")) : {};
+      let currentUser = users[user];
+      if (!currentUser) {
+        currentUser = {};
+      }
 
-    if (image === "") {
-    image = displayImage()
-    document.cookie = "earth_or_mars=" + image;
-    timesViewed = 1;
-    document.cookie = "image_views=" + timesViewed
-    } else {
-    const views = getCookie("image_views");
-    timesViewed = parseInt(views, 10) + 1;
-    document.cookie = "image_views=" + timesViewed;
+      let image = currentUser["earth_or_mars"];
+
+      if (!image) {
+        image = displayImage();
+        currentUser["earth_or_mars"] = image;
+        currentUser["image_views"] = 1;
+      } else {
+        // currentUser["earth_or_mars"] = displayImage();
+
+        currentUser["image_views"] =
+          parseInt(currentUser["image_views"], 10) + 1;
+      }
+      users[user] = currentUser;
+      document.cookie = "users=" + JSON.stringify(users);
+      return {
+        image: currentUser["earth_or_mars"],
+        timesViewed: currentUser["image_views"],
+      };
     }
 
-    return {image, timesViewed}
+  }
+  useEffect(() => {
+    let user = fire.auth()?.currentUser?.email;
+if(user){
+
+    const { image, timesViewed } = updateImageCookies(user);
+    setImage(image);
+    setTimesViewed(timesViewed);
+}else{
+    history.push('/');
 }
+  }, []);
 
-    const {image, timesViewed} = updateImageCookies()
+  // document.getElementsByClassName('home__image').src = image;
+  // document.getElementsByClassName('home__number').innerHTML = timesViewed
 
-    // document.getElementsByClassName('home__image').src = image;
-    // document.getElementsByClassName('home__number').innerHTML = timesViewed
-
-
-    return (
-        <section className="home">
-            <h1>Home</h1>
-            <button className="home__logout" onClick={handleLogout}>logout</button>
-            <div className="home__circle-container">
-        <img src={image} alt=""/>
-            {/* <div className="home__circle" style={{backgroundColor: image}}></div> */}
-            <p className="home__number">number of times you've visited: {timesViewed}</p>
-            </div>
-        </section>
-    )
+  return (
+    <section className="home">
+      <h1>Home</h1>
+      <button className="home__logout" onClick={handleLogout}>
+        logout
+      </button>
+      <div className="home__circle-container">
+        <img className="home__image" src={image} alt="" />
+        {/* <div className="home__circle" style={{backgroundColor: image}}></div> */}
+        <p className="home__number">
+          number of times you've visited: {timesViewed}
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default Home;
